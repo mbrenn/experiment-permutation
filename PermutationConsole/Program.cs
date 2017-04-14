67cd33db6c5ea3cb8b6b75611a8caabcc5a6cbf1
Martin Brenn <brenn@depon.net>
2017-04-14 11:44:47 +0200

    Projektdateien hinzufügen.

---
+++ PermutationConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PermutationTest;
+
+namespace PermutationConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var list = new[] {1, 2, 3, 4, 5};
+            foreach (var permutation in Permutation.GetAllPermutations(list))
+            {
+                foreach (var val in permutation)
+                {
+                    Console.Write(val + ", ");
+                }
+
+                Console.WriteLine();
+            }*/
+
+            for (var n = 1; n < 11; n++)
+            {
+                var largeList = new int[n];
+                for (var m = 0; m < n; m++)
+                {
+                    largeList[m] = m;
+                }
+
+                var watch = Stopwatch.StartNew();
+                var count = Permutation.GetAllPermutations(largeList).LongCount();
+                Console.WriteLine(count);
+                watch.Stop();
+                Console.WriteLine($"Elapsed: {watch.Elapsed} for {n} elements: " +
+                                  $"{count * 1000 / (watch.ElapsedMilliseconds+1)} elements/s");
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PermutationTest/Permutation.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PermutationTest
+{
+    public static class Permutation
+    {
+        /// <summary>
+        /// Gets all permutations of the given element as a 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<T[]> GetAllPermutations<T>(T[] source)
+        {
+            if (source.Length == 0)
+            {
+                yield break;
+            }
+            if (source.Length == 1)
+            {
+                yield return source.ToArray();
+            }
+            else if (source.Length == 2) // this are permutations of array of size 2
+            {
+                // Creates the two possibilities for permutation
+                yield return new[] {source[0], source[1]};
+                yield return new[] {source[1], source[0]};
+            }
+            else if (source.Length == 3) // this are permutations of array of size 3
+            {
+                // Creates the six possibilities for permutation
+                // Leads to a performance increase by 20%
+                yield return new[] { source[0], source[1], source[2] };
+                yield return new[] { source[0], source[2], source[1] };
+                yield return new[] { source[1], source[0], source[2] };
+                yield return new[] { source[1], source[2], source[0] };
+                yield return new[] { source[2], source[0], source[1] };
+                yield return new[] { source[2], source[1], source[0] };
+            }
+            else
+            {
+                var resultArray = new T[source.Length];
+                var subArray = new T[source.Length - 1];
+
+                for (var n = 0; n < source.Length; n++)
+                {
+                    // Creates a subArray for the given array. 
+                    var element = source[n];
+                    var i = 0;
+                    for (var m = 0; m < source.Length; m++)
+                    {
+                        if (m == n)
+                        {
+                            continue;
+                        }
+
+                        subArray[i] = source[m];
+                        i++;
+                    }
+
+                    resultArray[0] = element;
+                    //foreach (var permutedSubArrays in GetAllPermutations(subArray))
+                    Parallel.ForEach(GetAllPermutations<T>(subArray),
+                        permutedSubArrays =>
+                        {
+                            for (var m = 0; m < permutedSubArrays.Length; m++)
+                            {
+                                resultArray[m + 1] = permutedSubArrays[m];
+                            }
+
+                            yield return resultArray;
+                        });
+                }
+            }
+        }
+    }
+}

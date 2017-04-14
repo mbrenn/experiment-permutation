a54049f9787f1c5f5047d181d4b174e90c822149
Martin Brenn <brenn@depon.net>
2017-04-14 17:14:03 +0200

    Much faster due to parallel processing

--- PermutationTest/Permutation.cs
+++ PermutationTest/Permutation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,98 +13,90 @@ namespace PermutationTest
         /// <typeparam name="T"></typeparam>
         /// <param name="source"></param>
         /// <returns></returns>
-        public static IEnumerable<T[]> GetAllPermutations<T>(T[] source)
+        public static List<T[]> GetAllPermutations<T>(T[] source)
         {
-            if (source.Length == 0)
+            switch (source.Length)
             {
-                yield break;
+                case 0:
+                    return new List<T[]>();
+                case 1:
+                    return new List<T[]> {source.ToArray()};
+                case 2:
+                    // Creates the two possibilities for permutation
+                    return new List<T[]>
+                    {
+                        new[] {source[0], source[1]},
+                        new[] {source[1], source[0]}
+                    };
+                case 3:
+                    // Creates the six possibilities for permutation
+                    // Leads to a performance increase by 20%#
+                    return new List<T[]>
+                    {
+                        new[] {source[0], source[1], source[2]},
+                        new[] {source[0], source[2], source[1]},
+                        new[] {source[1], source[0], source[2]},
+                        new[] {source[1], source[2], source[0]},
+                        new[] {source[2], source[0], source[1]},
+                        new[] {source[2], source[1], source[0]},
+                    };
             }
-            if (source.Length == 1)
+
+            var result = new List<T[]>();
+            var resultArray = new T[source.Length];
+            var subArray = new T[source.Length - 1];
+            var first = true;
+            // for (var n = 0; n < source.Length; n++)
+
+            var func = new Action<int>(n =>
             {
-                yield return source.ToArray();
-            }
-            else if (source.Length == 2) // this are permutations of array of size 2
+                // Creates a subArray for the given array. 
+                var element = source[n];
+                var i = 0;
+                for (var m = 0; m < source.Length; m++)
+                {
+                    if (m == n)
+                    {
+                        if (first)
+                        {
+                            continue;
+                        }
+                        break;
+                    }
+
+                    subArray[i] = source[m];
+                    i++;
+                }
+                first = false;
+
+                resultArray[0] = element;
+                foreach (var permutedSubArrays in GetAllPermutations(subArray))
+                {
+                    for (var m = 0; m < permutedSubArrays.Length; m++)
+                    {
+                        resultArray[m + 1] = permutedSubArrays[m];
+                    }
+
+                    lock (result)
+                    {
+                        result.Add(resultArray.ToArray());
+                    }
+                }
+            });
+
+            if (source.Length > 7)
             {
-                // Creates the two possibilities for permutation
-                yield return new[] {source[0], source[1]};
-                yield return new[] {source[1], source[0]};
-            }
-            else if (source.Length == 3) // this are permutations of array of size 3
-            {
-                // Creates the six possibilities for permutation
-                // Leads to a performance increase by 20%
-                yield return new[] { source[0], source[1], source[2] };
-                yield return new[] { source[0], source[2], source[1] };
-                yield return new[] { source[1], source[0], source[2] };
-                yield return new[] { source[1], source[2], source[0] };
-                yield return new[] { source[2], source[0], source[1] };
-                yield return new[] { source[2], source[1], source[0] };
+                Parallel.For(0, source.Length, func);
             }
             else
             {
-                var resultArray = new T[source.Length];
-                var subArray = new T[source.Length - 1];
-                var first = true;
-
                 for (var n = 0; n < source.Length; n++)
                 {
-                    // Creates a subArray for the given array. 
-                    var element = source[n];
-                    var i = 0;
-                    for (var m = 0; m < source.Length; m++)
-                    {
-                        if (m == n)
-                        {
-                            if (first)
-                            {
-                                continue;
-                            }
-                            break;
-                        }
-
-                        subArray[i] = source[m];
-                        i++;
-                    }
-                    first = false;
-
-                    if (source.Length < 7)
-                    {
-                        resultArray[0] = element;
-                        foreach (var permutedSubArrays in GetAllPermutations(subArray))
-                        {
-                            for (var m = 0; m < permutedSubArrays.Length; m++)
-                            {
-                                resultArray[m + 1] = permutedSubArrays[m];
-                            }
-
-                            yield return resultArray;
-                        }
-                    }
-                    else
-                    {
-                        // Parallel
-                        var list = new List<T[]>();
-                        Parallel.ForEach(GetAllPermutations(subArray), x =>
-                        {
-                            for (var m = 0; m < x.Length; m++)
-                            {
-                                resultArray[m + 1] = x[m];
-                            }
-
-                            lock (list)
-                            {
-                                list.Add(resultArray.ToArray());
-                            }
-                        });
-
-                        foreach (var item in list)
-                        {
-                            yield return item;
-                        }
-                    }
-
+                    func(n);
                 }
             }
+
+            return result;
         }
     }
 }
